837662635597bb201ce2e7fb738384a6d3bb4556
jay <779878443@qq.com>
2021-01-11 18:40:01 +0800

    add file

--- app.py
+++ app.py
@@ -5,6 +5,16 @@
    File     : app.py
    Author   : CoderPig
    date     : 2021-01-11 18:15 
-   Desc     : 
+   Desc     : 程序运行类
 -------------------------------------------------
 """
+
+
+def download_bilibli_video():
+    print("未检测到cookie文件")
+    pass
+
+
+if __name__ == '__main__':
+    print("请输入您想执行操作的序号：")
+    print("1、B站视频下载字母提取一条龙")
